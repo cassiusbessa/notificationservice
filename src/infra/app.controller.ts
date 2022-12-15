9ee53716900f195c631438603ab896c0e0306f79
cassiusbessa <cassiusbessa@gmail.com>
2022-12-15 02:10:36 -0300

    feat: :sparkles: implements create and list all notifications controller

---
+++ src/infra/app.controller.ts
@@ -0,0 +1,27 @@
+import { PrismaService } from './prisma.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification-body';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { content, category, recipientId } = body;
+    return this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
