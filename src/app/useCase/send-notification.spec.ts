5c4a44e4673f1890c8a65e460031a1435d0fc31e
cassiusbessa <cassiusbessa@gmail.com>
2022-12-15 23:22:47 -0300

    test: :sparkles: implements abstract repository test

--- src/app/useCase/send-notification.spec.ts
+++ src/app/useCase/send-notification.spec.ts
@@ -1,15 +1,24 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const mockNotificationRepository = {
+  async create(notification: Notification): Promise<void> {
+    notifications.push(notification);
+  },
+};
 
 describe('SendNotification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(mockNotificationRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       recipientId: '1',
       content: 'Nova notificação',
       category: 'test',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
   });
 });
