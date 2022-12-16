e7b7eb47bc2f1256d304ff6963bf756bedd80e9c
cassiusbessa <cassiusbessa@gmail.com>
2022-12-16 20:01:06 -0300

    feat: :sparkles: implements count recipient id

---
+++ src/app/useCase/count-recipient-notification.ts
@@ -0,0 +1,28 @@
+import { NotificationRepository } from '../repositories/notifications-repository';
+import { Injectable } from '@nestjs/common';
+
+export interface CountRecipientNotificationRequest {
+  recipientId: string;
+}
+
+interface CountRecipientNotificationResponse {
+  count: number;
+}
+
+@Injectable()
+export class CountRecipientNotification {
+  constructor(
+    private readonly notificationsRepository: NotificationRepository,
+  ) {}
+  async execute(
+    request: CountRecipientNotificationRequest,
+  ): Promise<CountRecipientNotificationResponse> {
+    const { recipientId } = request;
+
+    const count = await this.notificationsRepository.countManyByRecipientId(
+      recipientId,
+    );
+
+    return { count };
+  }
+}
