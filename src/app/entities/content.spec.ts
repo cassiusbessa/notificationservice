895fbe04346d626be61390947a3cc13a4bee4c0d
cassiusbessa <cassiusbessa@gmail.com>
2022-12-16 17:38:15 -0300

    test: :bug: renames describe content test

--- src/app/entities/content.spec.ts
+++ src/app/entities/content.spec.ts
@@ -1,6 +1,6 @@
 import { Content } from './content';
 
-describe('Content', () => {
+describe('Notification Content', () => {
   it('should be able to create a notification content', () => {
     const content = new Content('Hello World');
     expect(content).toBeTruthy();
