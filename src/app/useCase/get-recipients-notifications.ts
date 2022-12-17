fd64418587c0137b206e5d15469749bb489349d8
cassiusbessa <cassiusbessa@gmail.com>
2022-12-17 01:37:13 -0300

    feat: :sparkles: implements get recipients notifications

---
+++ src/app/useCase/get-recipients-notifications.ts
@@ -0,0 +1,28 @@
+import { NotificationRepository } from '../repositories/notifications-repository';
+import { Injectable } from '@nestjs/common';
+import { Notification } from '../entities/notification';
+
+export interface GetRecipientNotificationRequest {
+  recipientId: string;
+}
+
+interface GetRecipientNotificationResponse {
+  notifications: Notification[];
+}
+
+@Injectable()
+export class GetRecipientNotification {
+  constructor(
+    private readonly notificationsRepository: NotificationRepository,
+  ) {}
+  async execute(
+    request: GetRecipientNotificationRequest,
+  ): Promise<GetRecipientNotificationResponse> {
+    const { recipientId } = request;
+
+    const notifications =
+      await this.notificationsRepository.findManyByRecipientId(recipientId);
+
+    return { notifications };
+  }
+}
