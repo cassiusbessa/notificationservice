dbf5a6691a228e295606bab35f135d92582f976f
cassiusbessa <cassiusbessa@gmail.com>
2022-12-17 02:06:33 -0300

    test: :sparkles: implements unread tests

---
+++ src/app/useCase/unreadNotification/unread-notification.spec.ts
@@ -0,0 +1,35 @@
+import { UnReadNotification } from './unread-notification';
+import { makeNotification } from '@test/factories/notification-factory';
+import { NotificationNotFound } from '../errors/notification-not-found';
+import { InMemoryNotificationsRepository } from '../../../../test/in-memory-notifications-repository';
+
+describe('Read Notification', () => {
+  it('should be able to read a notification', async () => {
+    const mockNotificationRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new UnReadNotification(
+      mockNotificationRepository,
+    );
+
+    const notification = makeNotification();
+    await mockNotificationRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(mockNotificationRepository.notifications[0].readAt).toBe(null);
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const mockNotificationRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new UnReadNotification(
+      mockNotificationRepository,
+    );
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
