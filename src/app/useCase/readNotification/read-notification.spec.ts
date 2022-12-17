5cb4dae230a3a1d4b1b7dfb8d552b0bea5e065e8
cassiusbessa <cassiusbessa@gmail.com>
2022-12-17 01:58:45 -0300

    test: :sparkles: implements read notification test

---
+++ src/app/useCase/readNotification/read-notification.spec.ts
@@ -0,0 +1,33 @@
+import { ReadNotification } from './read-notification';
+import { makeNotification } from '@test/factories/notification-factory';
+import { NotificationNotFound } from '../errors/notification-not-found';
+import { InMemoryNotificationsRepository } from '../../../../test/in-memory-notifications-repository';
+
+describe('Read Notification', () => {
+  it('should be able to read a notification', async () => {
+    const mockNotificationRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new ReadNotification(mockNotificationRepository);
+
+    const notification = makeNotification();
+    await mockNotificationRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(mockNotificationRepository.notifications[0].readAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const mockNotificationRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new ReadNotification(mockNotificationRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
