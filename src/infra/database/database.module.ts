eb0dd38813c4ebae743173424091d84d3d9ad637
cassiusbessa <cassiusbessa@gmail.com>
2022-12-17 02:20:19 -0300

    refactor: :art: segregates prisma db file in a folder

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { NotificationRepository } from 'src/app/repositories/notifications-repository';
 import { PrismaNotificationsRepository } from '../repositories/prisma-notifications-repository';
-import { PrismaService } from './prisma.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   providers: [
