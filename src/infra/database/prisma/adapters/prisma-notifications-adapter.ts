162929bf32f6a650cc027f689759438287aaaad0
cassiusbessa <cassiusbessa@gmail.com>
2022-12-17 03:11:03 -0300

    feat: :art: implements adaptor prisma raw to domain

--- src/infra/database/prisma/adapters/prisma-notifications-adapter.ts
+++ src/infra/database/prisma/adapters/prisma-notifications-adapter.ts
@@ -1,4 +1,6 @@
 import { Notification } from 'src/app/entities/notification';
+import { Notification as rawNotification } from '@prisma/client';
+import { Content } from 'src/app/entities/content';
 
 export class PrismaNotificationAdpter {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,19 @@ export class PrismaNotificationAdpter {
       recipientId,
       cancelledAt,
     };
+  }
+
+  static toDomain(prismaData: rawNotification): Notification {
+    return new Notification(
+      {
+        category: prismaData.category,
+        content: new Content(prismaData.content),
+        recipientId: prismaData.recipientId,
+        readAt: prismaData.readAt,
+        cancelledAt: prismaData.cancelledAt,
+        createdAt: prismaData.createdAt,
+      },
+      prismaData.id,
+    );
   }
 }
